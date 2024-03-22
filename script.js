d17758931fc9fd99bdb4b1bee899ae81b6e12576
Joydeep Sarkar <138491246+joydeepsarkar99@users.noreply.github.com>
2024-03-22 12:38:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+function mincost(arr){ 
+	let cost = 0
+	while(arr.length > 1){
+		arr.sort(function(a,b){return a - b})
+		let merge = arr.shift() + arr.shift()
+		arr.unshift(merge)
+		cost += merge
+	}
+	return cost
 }
 
 module.exports=mincost;
